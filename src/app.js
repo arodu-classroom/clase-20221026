34bb8a3b48a26f23959b81cc50131f964d5f86ad
Alberto Rodriguez <arodu.dev@gmail.com>
2022-10-24 09:40:25 -0400

    nueva ruta home

--- src/app.js
+++ src/app.js
@@ -6,4 +6,8 @@ app.get('/', function (req, res) {
     res.send("<h1>Hola Mundo!!!</h1>")
 })
 
+app.get('/home', function (req, res) {
+    res.send("<h1>Otra ruta!!!</h1>")
+})
+
 app.listen(process.env.PORT || 3000);   
