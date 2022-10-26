c86b3faffd441cbe37c73458a5e36944f6b0021e
Alberto Rodriguez <arodu.dev@gmail.com>
2022-10-26 09:00:13 -0400

    actualizacion README

---
+++ README.md
@@ -0,0 +1,12 @@
+# Clase 2022-10-26
+
+## Instalaciones
+- NodeJs
+    - https://nodejs.org/es/
+
+- express
+    - https://expressjs.com/
+    - https://expressjs.com/en/starter/generator.html
+
+- Git
+    - https://git-scm.com/
--- src/app.js
+++ src/app.js
@@ -3,11 +3,7 @@ import express from "express"
 const app = express()
 
 app.get('/', function (req, res) {
-    res.send("<h1>Hola Mundo!!!</h1>")
+    res.send("<h1>Hello World!!!</h1>")
 })
 
-app.get('/home', function (req, res) {
-    res.send("<h1>Otra ruta!!!</h1>")
-})
-
-app.listen(process.env.PORT || 3000);   
+app.listen(process.env.PORT || 3000);
